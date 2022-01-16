ff42ac2051b6fd0aefcbd275e3343614d0b716eb
JorgeCarlotto <jorgemcarlotto@gmail.com>
2022-01-16 10:12:19 -0300

    Profile componet - React Typical ok - Cv download ok

--- src/PorfolioContainer/Home/Profile.js
+++ src/PorfolioContainer/Home/Profile.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from "react";
+import Typical from "react-typical";
 
 export default function Profile() {
-    return (
-        <div>
-            
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-detailes">
+          <div className="colz">
+              <div className="colz-icon">            
+            <a href="#">
+              <i className="fa fa-facebook"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-google-plus"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-youtube"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-twitter"></i>
+            </a>
+            </div>
+          </div>
+        
+        <div className="profile-details-name">
+            <span className="primary-text">
+                {" "}
+                Hola, Soy <span className="highlighted-text">Jorge Carlotto</span>
+            </span>
         </div>
-    )
+        <div className="profile-details-role">
+            <span className="primary-text">
+                {" "}
+                <h1>
+                   {" "}
+                   <Typical 
+                   loop ={Infinity}
+                   steps={[
+                       "Desarrollador Junior 🚀",
+                       1000,
+                       "Full Stack Developer 💻 ",
+                       1000,
+                       "Node Js Dev 🤓",
+                       1000,
+                       "React Dev 😎",
+                       1000,
+                   ]}
+                   /> 
+                </h1>
+                <span className="profile-role-tagline">
+                lorem ipsum dolor sit amet, consectetur adip.
+                </span>
+            </span>
+        </div>
+        <div className="profile-options">
+            <button className="btn primary-btn">
+                {" "}
+                Contactame{" "}
+            </button>
+            <a href="Cv-Jorge Carlotto.pdf" download="Cv-Jorge Carlotto.pdf">
+                <button className="btn highlighted-btn">Mi Curriculum</button>
+            </a>
+        </div>
+        </div>
+        <div className="profile-picture">
+            <div className="profile-background"></div>     
+        </div>
+      </div>
+    </div>
+  );
 }
